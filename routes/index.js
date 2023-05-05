5b80be711aa85ab351138ef03b0178990861b6c9
Petr Masol <petyamasol@gmail.com>
2023-05-05 13:45:49 +0300

    feat: Joi router

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,15 @@ const {
 } = require('../controllers/users');
 const { auth } = require('../middlewares/auth');
 const NotFoundError = require('../utils/error/not-found');
+const {
+  validateLoginData, validateRegisterData,
+} = require('../middlewares/validation/usercelebrate');
 
 const routerUsers = require('./users');
 const routerCards = require('./cards');
 
-router.post('/signin', login);
-router.post('/signup', createUsers);
+router.post('/signin', validateLoginData, login);
+router.post('/signup', validateRegisterData, createUsers);
 router.use('/users', auth, routerUsers);
 router.use('/cards', auth, routerCards);
 router.use('*', auth, (req, res, next) => {
